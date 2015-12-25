6adb368d3f4dd6b74c60a20113019356f164fa17
Ashish Mishra <ashish@gooru.org>
2015-12-25 12:34:12 +0530

    Initial repo creation

---
+++ src/main/java/org/gooru/nucleus/handlers/collections/bootstrap/CollectionVerticle.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.collections.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class CollectionVerticle extends AbstractVerticle {
+}
