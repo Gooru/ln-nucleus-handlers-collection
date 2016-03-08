26201cff9a53882f196ba269c0b9bfe9679295bd
am-gooru <ashish@gooru.org>
2016-03-08 16:10:34 +0530

    Provide granular event names to different operations

--- src/main/java/org/gooru/nucleus/handlers/collections/processors/events/EventBuilderFactory.java
+++ src/main/java/org/gooru/nucleus/handlers/collections/processors/events/EventBuilderFactory.java
@@ -10,7 +10,9 @@ public final class EventBuilderFactory {
   private static final String EVT_COLLECTION_CREATE = "event.collection.create";
   private static final String EVT_COLLECTION_UPDATE = "event.collection.update";
   private static final String EVT_COLLECTION_DELETE = "event.collection.delete";
-  private static final String EVT_COLLECTION_ADD_CONTENT = "event.collection.add.content";
+  private static final String EVT_COLLECTION_CONTENT_ADD = "event.collection.content.add";
+  private static final String EVT_COLLECTION_REORDER = "event.collection.reorder";
+  private static final String EVT_COLLECTION_COLLABORATOR_UPDATE = "event.collection.collaborator.update";
   private static final String EVENT_NAME = "event.name";
   private static final String EVENT_BODY = "event.body";
   private static final String COLLECTION_ID = "id";
@@ -32,15 +34,16 @@ public final class EventBuilderFactory {
   }
 
   public static EventBuilder getAddContentToCollectionEventBuilder(String collectionId) {
-    return () -> new JsonObject().put(EVENT_NAME, EVT_COLLECTION_ADD_CONTENT).put(EVENT_BODY, new JsonObject().put(COLLECTION_ID, collectionId));
+    return () -> new JsonObject().put(EVENT_NAME, EVT_COLLECTION_CONTENT_ADD).put(EVENT_BODY, new JsonObject().put(COLLECTION_ID, collectionId));
   }
 
   public static EventBuilder getReorderContentEventBuilder(String collectionId) {
-    return () -> new JsonObject().put(EVENT_NAME, EVT_COLLECTION_UPDATE).put(EVENT_BODY, new JsonObject().put(COLLECTION_ID, collectionId));
+    return () -> new JsonObject().put(EVENT_NAME, EVT_COLLECTION_REORDER).put(EVENT_BODY, new JsonObject().put(COLLECTION_ID, collectionId));
   }
 
   public static EventBuilder getUpdateCollaboratorEventBuilder(String collectionId) {
-    return () -> new JsonObject().put(EVENT_NAME, EVT_COLLECTION_UPDATE).put(EVENT_BODY, new JsonObject().put(COLLECTION_ID, collectionId));
+    return () -> new JsonObject().put(EVENT_NAME, EVT_COLLECTION_COLLABORATOR_UPDATE)
+                                 .put(EVENT_BODY, new JsonObject().put(COLLECTION_ID, collectionId));
   }
 
 
